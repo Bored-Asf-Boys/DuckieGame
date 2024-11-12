e8e44242db982ad209ff87fa00ecb1c8dbcd1aac
Ethanscharlie <96396138+Ethanscharlie@users.noreply.github.com>
2024-11-12 08:53:52 -0700

    Basic white rect

--- src/Runner.java
+++ src/Runner.java
@@ -1,12 +1,32 @@
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.*;
 
 public class Runner
 {
+	static public JFrame frame;
+	static public JPanel jp;
+	
+	public static void drawRect(int x, int y, int w, int h)
+	{
+		JPanel rectangle = new JPanel();   
+        rectangle.setBackground( Color.white);   
+        rectangle.setBounds(x, y, w, h);
+        jp.add(rectangle);
+        frame.setVisible(true);
+	}
+	
 	public static void main(String args[])
 	{
-        JFrame frame = new JFrame("My Window");
-        frame.setSize(400, 300);
+		frame = new JFrame("I'm a JFrame");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        frame.setSize(1000, 1000);
+        jp = new JPanel();
+        jp.setLayout(null);
+        jp.setBackground(Color.black);
+        frame.add(jp);
+        
+        drawRect(50, 50, 60, 30);
 	}
 }
